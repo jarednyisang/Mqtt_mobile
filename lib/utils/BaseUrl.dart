3659245770c51d8d56567247af297a362b2831ba
Jared Nyisang <jarednyisang@gmail.com>
2025-12-16 10:01:24 +0300

    good progress

--- lib/utils/BaseUrl.dart
+++ lib/utils/BaseUrl.dart
@@ -6,28 +6,13 @@ class BaseUrl {
   static const String SENDEMAILOTP = "$BASE_URL/api/sendPasswordMessage";
   static const String UPDATEPASSWORD = "$BASE_URL/api/updatepassword";
   static const String GETCOUNTRIES = "$BASE_URL/api/fetchcountries";
-    static const String GETDONATIONAMOUNT = "$BASE_URL/api/fetchprice";
 
-  static const String AVAILABLESURVEYS = "$BASE_URL/api/availablesurveys";
-  static const String COMPLETEDSURVEYS = "$BASE_URL/api/completedsurveys";
    static const String DASHBOARD = "$BASE_URL/api/dashboard";
-  static const String TRANSACTION = "$BASE_URL/api/transactions";
-  static const String REFFERALS = "$BASE_URL/api/referrals";
-  static const String WITHDRAW = "$BASE_URL/api/withdraw";
-  static const String POSTRESPONSE = "$BASE_URL/api/createfeedback";
- // Add these to your BaseUrl class:
-  static const String UPDATE_USER_DONATION = '$BASE_URL/api/revenuecat/updateSubscription';
-  static const String USER_DONATION_HISTORY = '$BASE_URL/api/revenuecat/subscriptionHistory';
-  static const String GET_USER_BY_ID = '$BASE_URL/api/revenuecat/getUserById';
-  static const String ACTIVATE_MANUALLY = '$BASE_URL/api/revenuecat/activateManually';
-  
 
 
 
-static const String PESAPALSUBMITORDER = '$BASE_URL/api/pesapal/submit-order';
-static const String PESAPALCALLBACK = '$BASE_URL/api//pesapal/callback';
-static const String PESAPALIPN = '$BASE_URL/api/pesapal/ipn';
-static const String PESAPALCHECKSTATUS = '$BASE_URL/api/pesapal/check-status';
+
+
 
 
 }
